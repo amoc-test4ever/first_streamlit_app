a8df2619e8c435707ffc8c910904416d3e8ac738
PoojaD <121683271+amoc-test4ever@users.noreply.github.com>
2023-01-07 17:31:26 +0530

    Update streamlit_app.py
    added text stat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.title('My Parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 and Blueberry oat Meal')
+st.text('kale spianch and Rocket Smoothis')
+st.text('Hard-boled free range Egg')
