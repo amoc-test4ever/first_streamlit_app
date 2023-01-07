0d1685cdb6409908dc3508b7f2208b5b256272f6
PoojaD <121683271+amoc-test4ever@users.noreply.github.com>
2023-01-07 17:35:24 +0530

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.title('My Parents New Healthy Diner')
+   
 st.header('Breakfast Menu')
-st.text('Omega 3 and Blueberry oat Meal')
-st.text('kale spianch and Rocket Smoothis')
-st.text('Hard-boled free range Egg')
+st.text('🥣 Omega 3 and Blueberry oat Meal')
+st.text('🥗 kale spianch and Rocket Smoothis')
+st.text('🐔 Hard-boled free range Egg')
+st.text('🥑🍞 Avacado Toast')
