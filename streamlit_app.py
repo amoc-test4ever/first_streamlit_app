652caa9c33361347ece62bc0fee7d355345fc04d
PoojaD <121683271+amoc-test4ever@users.noreply.github.com>
2023-01-15 21:03:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas
 import requests
 import snowflake.connector
-from urlib.error import URLError
+from urllib.error import URLError
 
 
 st.title('My Parents New Healthy Diner')
