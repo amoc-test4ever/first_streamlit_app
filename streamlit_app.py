8a98ccdd52587b965f81859e9d8dea78c1ab6fad
PoojaD <121683271+amoc-test4ever@users.noreply.github.com>
2023-01-15 21:05:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
 
+st.stop()
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
