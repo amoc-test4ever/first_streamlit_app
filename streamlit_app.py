e88141cbad221e275aa6794300b20008bac804be
PoojaD <121683271+amoc-test4ever@users.noreply.github.com>
2023-01-15 21:48:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,77 +1,54 @@
-
-
-
 import streamlit as st
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
-st.title('My Parents New Healthy Diner')
-   
-st.header('Breakfast Menu')
-st.text('🥣 Omega 3 and Blueberry oat Meal')
-st.text('🥗 kale spianch and Rocket Smoothis')
-st.text('🐔 Hard-boled free range Egg')
-st.text('🥑🍞 Avacado Toast')
-
+st.title ("My MOM's new healthy Dinner")
+st.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu 🥣 🥗 🐔 🥑🍞')
+st.text('🥗 Curry Omega 3 & Blueberry Oatmeal')
+st.text('🥑🍞 Sandwich Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Cooking Hard-Boiled Free-Range Egg')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = st.multiselect("Pick Some Fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected= st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruit_to_show = my_fruit_list.loc[fruits_selected]
+st.dataframe(fruit_to_show)
 
-
-# Display the table on the page.
-st.dataframe(fruits_to_show)
-
-
+# create a repeated code boack
 def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice )
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   return fruityvice_normalized
-
-
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)            
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  # new  section to display fruityvice api  response
 st.header("Fruityvice Fruit Advice!")
-   
 try:
-   fruit_choice = st.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-      st.error("Please select  a fruit to get information")
-   else:
-         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice )
-         #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-         #st.dataframe(fruityvice_normalized)
-         back_from_function= get_fruityvice_data(fruit_choice)
-         st.dataframe(back_from_function)
+    fruit_choice = st.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        st.error('Please select a fruit to get information.')
+    else:
+        back_from_function=get_fruityvice_data(fruit_choice)
+        st.dataframe(back_from_function)
+                                 
 except URLError as e:
-    st.error()
-   
-#st.write('The user entered ', fruit_choice)
-# st.text(fruityvice_response)
-# write your own comment -what does the next line do? 
-# write your own comment - what does this do?
+    #st.error()                            
+st.stop();
+st.header("The Fruit Load List Contains:")
+#Snowflake related function
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from fruit_load_list")
+       return my_cur.fetchall()
 
-
-
-
-st.stop()
-
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT * from fruit_load_list")
-my_cur.execute("insert into fruit_load_list values ('from st')")
-my_data_rows = my_cur.fetchall()
-st.header("The fruit load list contains:")
-st.dataframe(my_data_rows)
-
+#Add button to load the fruit
+if st.button('Get Fruit Load List:'):
+   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+   my_data_rows= get_fruit_load_list()
+   st.dataframe(my_data_rows)
 
 add_my_fruit = st.text_input('What fruit would you like to add?','jackfruit')
 st.write('The user entered ', add_my_fruit)
-
-
