28fd280705c34d884d2879d4d13bea51a375fae2
PoojaD <121683271+amoc-test4ever@users.noreply.github.com>
2023-01-15 21:10:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,13 +31,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
+try:
 
-fruit_choice = st.text_input('What fruit would you like information about?','apple')
-st.write('The user entered ', fruit_choice)
+fruit_choice = st.text_input('What fruit would you like information about?')
+if not fruit_choice:
+   st.error('Please select  a fruit to get information')
+  else:
+   
+#st.write('The user entered ', fruit_choice)
 
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "apple")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice )
 # st.text(fruityvice_response)
 
 # write your own comment -what does the next line do? 
@@ -45,6 +48,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
+expect URLError as e:
+   st.error()
 
 st.stop()
 
